f5d3fd9b1865655c2b2bf2af8d1ddac8850e9429
Rebecca Lacer <r.a.lacer@hotmail.com>
2021-04-13 13:59:56 -0600

    updates to writefile

--- index.js
+++ index.js
@@ -1,80 +1,96 @@
-// TODO: Include packages needed for this application
+// node modules
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-// const questions = [];
-
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-// function init() {}
-
-// Function call to initialize app
-// init();
-
-const inquirer = require("inquirer");
-const fs = require("fs");
-const path = require("path");
-const questions = [
-    {
-        type: "input",
-        name: "name",
-        message: "Enter your project title",   
-       },
-   
-    {
-     type: "input",
-     name: "name",
-     message: "Who is the author of this project?",   
-    },
-    {
-    type: "checkbox",
-    name: "language",
-    message: "What technologies were used?",
-    choices: ["html", "javascript", "css",  "nodejs"],   
-    },
-    {
-        type: "checkbox",
-        name: "licenses",
-        message: "Which licenses would you like to use?",
-        choices: ["Academic Free License v3.0", "Apache 2.0", "Artistic 2.0",  "Educational Community License 2.0", "GNU General Public License Family", "MIT", "Mozilla Public License 2.0", "The Unlicense", ],   
-        },
+inquirer.prompt(
+    [
         {
             type: "input",
-            name: "contributors",
-            message: "Who contributed?",
-              
-            },
+            name: "name",
+            message: "Enter your project title",
+            // check if user entered a value
+            validate: (value)=>{if(value){return true
+            } else {return 'I need a value to continue'}},
+           },
+           {
+            type: "input",
+        message: "Enter a description for your project",
+        name: "description",
+     },
+     {
+        type: "input",
+    message: "How do you install you app?",
+    name: "installation",
+ },
+     {
+        type: "input",
+    message: "How is your app used?",
+    name: "usage",
+ },
 
-    {
-type: "input",
-name: "communication",
-message: "What the code to  run a test?",
-    },
-    {
-        type: 'input',
-        message: 'What is your Github URL?',
-        name: 'Github',
-  
-      },
-      {
-        type: 'input',
-        message: 'What is your email?',
-        name: 'email',
-  
-      },
-]
+        {
+            type: "input",
+        message: "Who contributed to your project?",
+        name: "contributors",
+     },
+     {
+        type: "checkbox",
+        name: "license",
+        message: "Which license(s) do you need?",
+        choices: ["Academic Free License v3.0", "Apache 2.0", "Artistic 2.0",  "Educational Community License 2.0", "GNU General Public License Family", "MIT", "Mozilla Public License 2.0", "The Unlicense", "other"," N/A"],   
+        },
+        {
+            type: 'input',
+            message: 'What is your Github URL?',
+            name: 'github',
+      
+          },
+          {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'email',
+      
+          },
 
-function init() {
-inquirer.prompt(questions)
-.then(function (inquirerResponse) {
-    console.log(inquirerResponse)
-    const filename = `${inquirerResponse.name.toLowerCase().split(" ").join("")}Readmd.md`
-    fs.writeFile(filename, JSON.stringify(inquirerResponse, null, "\t"), function(err) {
-        err ? console.log(err) : console.log("success");
-    })
-})
+    ]
+)
+.then(({
+title,
+description,
+installation,
+usage,
+contributors,
+license,
+github,
+email
+})=>{
+//   create the document template
+const template = `# ${title}
 
+*[description](#description)
+*[installation](#installation)
+*[usage](#usage)
+*[contributors](#contributors)
+*[license](#license)
+##${license}
+##${description}
+##${usage}
+##${installation}
+###${contributors}
+
+# Contact Me With Questions:
+*Github: ${github}
+*Email: ${email}`;
+// function to create readme file using fs
+createNewfile(title,template);
 }
+);
+function createNewfile(fileName,data){
 
-init()
+ fs.writeFile(`./README.md`,data,(err)=>{
+if(err){
+    console.log(err)
+}
+console.log("success")
+ })  
+}
